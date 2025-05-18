a97357e8a842c331fc8296efff7cd445710caec4
Your Name <you@example.com>
2025-05-18 14:38:02 -0400

    Add Glitch deployment configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
+  // Allows content to be served on Glitch
+  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
 }
 
 module.exports = nextConfig 
